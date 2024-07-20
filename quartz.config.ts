e7e1806efa734abbc3f8f086557603a6b8a296b8
Brandon <brandon@friendlystudios.net>
2024-07-20 17:00:25 -0500

    updated emojis in config

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "👁🗢👁 IonTheDev",
+    pageTitle: "👁️👄👁️ IonTheDev",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
